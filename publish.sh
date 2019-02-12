ee862b9c6eea5c32ed7b4dd0e93626299d162cd9
Raony Marcondes <raonymarcondes@gmail.com>
2019-02-12 15:33:54 -0200

    Add publish.sh

---
+++ publish.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+hugo
+git subtree split --prefix public -b master # create a local master branch containing the splitted output folder
+git push -f origin master:master # force the push of the master branch to the remote master branch at origin
+git branch -D master # delete the local master because you will need it: ref
